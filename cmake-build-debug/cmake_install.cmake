57562e9621a44495ef99abdf3c697d649c731369
JaaumG <jaaumgui@gmail.com>
2023-03-20 15:13:08 -0300

    Aperfeiçoado o algoritmo

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Jaaum/CLionProjects/untitled9
+# Install script for directory: C:/Users/Jaaum/CLionProjects/palindromo-com-pilha-c
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Jaaum/CLionProjects/untitled9/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Jaaum/CLionProjects/palindromo-com-pilha-c/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -11,57 +11,50 @@ typedef struct {
 
 typedef no * pilha;
 
-void criar(pilha * topo){
-    *topo = NULL;
+void criar(pilha * topo){ //função para criar a pilha
+    *topo = NULL; //inicializa a pilha
 }
 
-bool push(pilha * topo, char letra){
-    pilha novo = (pilha)malloc(sizeof (no));
-    if(novo!=NULL){
-        novo->letra = letra;
-        novo->prox = *topo;
-        *topo = novo;
+bool push(pilha * topo, char letra){ //função para adicionar um elemento na pilha
+    pilha novo = (pilha)malloc(sizeof (no)); //aloca um novo no
+    if(novo!=NULL){ //verifica se o no foi alocado
+        novo->letra = letra; //adiciona a letra no no
+        novo->prox = (struct no *) *topo; //adiciona o proximo no
+        *topo = novo; //adiciona o no na pilha
         return true;
     }
     return false;
 }
 
-bool pop(pilha * topo, char * letra){
-    if(*topo!=NULL){
-        pilha aux = *topo;
-        *letra = aux->letra;
-        *topo = aux->prox;
-        free(aux);
+bool pop(pilha * topo, char * letra){ //função para remover um elemento da pilha
+    if(*topo!=NULL){ //verifica se a pilha não está vazia
+        pilha aux = *topo; //cria um auxiliar para a pilha
+        *letra = aux->letra; //adiciona a letra no auxiliar
+        *topo = (pilha) aux->prox; //adiciona o proximo no
+        free(aux); //libera o no da memoria
         return true;
     }
     return false;
 }
 
-bool migrate(pilha * topo1, pilha * topo2, char * letra){
-    if(*topo1 != NULL && pop(topo1, letra) && push(topo2, *letra)) {
-        return true;
-    }
-    return false;
-}
-
-void exibir(pilha * topo){
-    if(*topo!=NULL){
-        pilha aux = *topo;
-        while(aux!=NULL){
-            printf("%c", aux->letra);
-            aux = aux->prox;
+bool clone(pilha * topo1, pilha * topo2){ //função para clonar uma pilha
+    if(*topo1!=NULL){ //verifica se a pilha não está vazia
+        pilha aux = *topo1; //cria um auxiliar para a pilha
+        while(aux!=NULL){ //percorre a pilha
+            push(topo2, aux->letra); //adiciona a letra no no
+            aux = (pilha) aux->prox; //adiciona o proximo no
         }
-        printf("\n");
+        return true;
     }
+    return false;
 }
 
-
-bool palindromo(pilha * topo1, pilha * topo2){
-    for (int i = 0; i < 255; ++i) {
-        char letra01, letra02;
-        pop(topo1, &letra01);
-        pop(topo2, &letra02);
-        if(letra01 != letra02){
+bool isPalindromo(pilha * topo1, pilha * topo2){ //função para verificar se é um palindromo
+     while(*topo1!=NULL){ //percorre a pilha
+        char letra01, letra02; //variaveis para armazenar as letras
+        pop(topo1, &letra01); //remove a letra da pilha
+        pop(topo2, &letra02); //remove a letra da pilha
+        if(letra01 != letra02){ //verifica se as letras são iguais
             return false;
         }
     }
@@ -69,34 +62,23 @@ bool palindromo(pilha * topo1, pilha * topo2){
 }
 
 int main (){
-    int opcao = 0;
-    char frase[255], frasereduzida[255];
-    pilha topo, topo2;
-    criar(&topo);
-    criar(&topo2);
-    printf("\nSistema para verificar palindromos");
-    printf("\nDigite o texto para verificar se é um palindromo.");
-    scanf("%[^\n]", frase);
-    int c = 0;
-    for (int i = 0; i < strlen(frase); ++i) {
-        if (isalnum(frase[i])){
-            frasereduzida[c] = frase[i];
-            c++;
+    char frase[255];//tamanho da frase
+    pilha topo, topo2; //instanciação da pilha
+    criar(&topo); //criação da pilha
+    criar(&topo2); //criação da pilha
+    printf("\nSistema para verificar palindromos.");
+    printf("\nDigite o texto para verificar se e um palindromo.\n");
+    scanf("%[^\n]", frase); //leitura da frase com metodo de leitura de string com espaço
+    for (int i = 0; i < strlen(frase); ++i) { //percorre a frase
+        if (isalnum(frase[i])){ //verifica se é um caracter alfanumerico
+            push(&topo, frase[i]); //adiciona a pilha
         }
     }
-    frasereduzida[c] = '\0';
-    printf("%s", frasereduzida);
-    for (int i = 0; i < strlen(frasereduzida) ; ++i) {
-        push(&topo, frasereduzida[i]);
-        push(&topo2, frasereduzida[strlen(frasereduzida) - (i+1)]);
-    }
-        exibir(&topo);
-        exibir(&topo2);
-    if (palindromo(&topo, &topo2)) {
-        printf("\nE um palindromo.");
+    clone(&topo, &topo2); //clona a pilha para outra pilha
+    if (isPalindromo(&topo, &topo2)) { //verifica se é um palindromo ou não
+        printf("\nA frase: %s e um palindromo.", frase);
     } else {
-        printf("\nNão é um palindromo.");
+        printf("\nA frase: %s nao e um palindromo.", frase);
     }
-
     return 0;
 }
